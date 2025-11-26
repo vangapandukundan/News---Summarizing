40721d2e495fd78f08d9c2558e75be4eb17b8d35
V.KUNDAN <159156743+vangapandukundan@users.noreply.github.com>
2025-11-26 12:52:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Get your free API key from: https://newsapi.org
+
+const CONFIG = {
+    NEWS_API_KEY: 'f2f42aaa969542e6baad4249a7c717d4'
+    
+};
+
+// Make it available globally
+window.CONFIG = CONFIG;
